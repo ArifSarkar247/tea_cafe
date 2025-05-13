cf6ffa8e99d98057e5f5e34e2ce18b9203e4f6eb
Arif Sarkar <s.a.arifsarkar24724@gmail.com>
2025-05-13 12:07:57 +0600

    Add Procfile and runtime.txt, set DEBUG=False

--- tea_cafe/settings.py
+++ tea_cafe/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*_@05f=$#*-l36^82w%g#xt^tw5!p^vk63j=__en7^nd!-jtg='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '172.20.234.94']
 
